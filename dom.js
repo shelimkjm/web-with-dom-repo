47b43d333d56a306059d4218906f0738dd35bc1d
shelim <shelimkjm@gmail.com>
2024-02-21 14:39:13 +0600

    added dom

--- dom.js
+++ dom.js
@@ -3,7 +3,7 @@ const scrollButton = document.getElementById('first-btn');
 const goalElement = document.getElementById('scroll');
 
 
-first-btn.addEventListener('click', function() {
+scrollButton.addEventListener('click', function() {
 
     goalElement.scrollIntoView({ behavior: 'smooth' });
 });
@@ -12,10 +12,41 @@ first-btn.addEventListener('click', function() {
 
 
 
-document.getElementsByClassName('btn').addEventListener('click', function(){
-for ( const singleBtn of btn){
-    console.log(singleBtn)
-}
+// document.getElementsByClassName('btn').addEventListener('click', function(){
+// for ( const singleBtn of btn){
+//     console.log(singleBtn)
+// }
 
-})
+// })
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.button-but');
+    const selectDown = document.getElementById('select-down');
+    const seatAdd = document.getElementById('seat-add');
+    const totalPrice = document.getElementById('total-price');
+    
+    let total = 0;
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+        selectDown.textContent = parseInt(selectDown.textContent) - 1;
+    seatAdd.textContent = parseInt(seatAdd.textContent) + 1;
+
+           total += 550;
+            totalPrice.textContent = total;
+        });
+    });
+});
+
+
+
+
+   
+
+
+
 
